3d40cce7d9c2fb68b53cc0c31d5325c8842322c2
AdithyaOasis1 <adithya.oasis@i.curefit.com>
2022-11-18 08:09:51 +0530

    extra

--- index.py
+++ index.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 import threading
 import os
 from jinja2 import Environment, FileSystemLoader
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def getAllWorkouts():
     environment = Environment(loader=FileSystemLoader("templates/"))
-    # template = environment.get_template("workouts.html")
-    return render_template("workouts.html", workouts=[{"userId": "adithya", "reps": 3}])
+    with open('DB.json', 'r') as json_file:
+        data = json.load(json_file)
+    return render_template("workouts.html", workouts=data)
 
 
 @app.route('/user')
