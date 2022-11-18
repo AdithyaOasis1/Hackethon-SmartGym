e2ab69b9b3766a329ae78127ce1da74b1c95d4f3
AdithyaOasis1 <adithya.oasis@i.curefit.com>
2022-11-18 07:42:31 +0530

    Added jinja

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import threading
 import os
-from image_tracker_func import runTracker
+from jinja2 import Environment, FileSystemLoader
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/')
+def getAllWorkouts():
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    # template = environment.get_template("workouts.html")
+    return render_template("workouts.html", workouts={"userId": "adithya", "reps": 3})
+
+
+@app.route('/user')
 def catch_all(path):
     return 'You want path: %s' % path
 
